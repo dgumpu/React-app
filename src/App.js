a36a2ffb09241c510e7f5bd6f89f816e3c682580
dgumpu <devgump07@gmail.com>
2018-07-19 10:22:19 -0500

    Refactored code .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import Posts from './components/Posts';
 
 class App extends Component {
+  state = { posts: [] };
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(
+        (posts) => this.setState({ posts: posts.data }));
+  }
   
   render() {
     return (
       <div className="container">
-        <Posts />
+        <h1 className="display-1">Post Samples</h1>
+        <Posts posts={this.state.posts} />
       </div>
     );
   }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,42 +1,26 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-class Posts extends Component {
-    state = { posts: [] };
-
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(
-            (posts) => this.setState({ posts: posts.data }));
-    }
-
-    render() {
-        return (
-            <div>
-                <h1 className="display-1">Post Samples</h1>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Title</th>
-                            <th>User Id</th>
-                            <th>Size</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.posts.map(post => (
-                            <tr>
-                                <td>{post.id}</td>
-                                <td>{post.title}</td>
-                                <td>{post.userId}</td>
-                                <td>{post.title.length}</td>
-                            </tr>
-                        ))}
-                    </tbody>    
-                </table>
-                
-            </div>
-        );
-    }
-}
+const Posts = props => (
+    <table className="table">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>User Id</th>
+                <th>Size</th>
+            </tr>
+        </thead>
+        <tbody>
+            {props.posts.map(post => (
+                <tr>
+                    <td>{post.id}</td>
+                    <td>{post.title}</td>
+                    <td>{post.userId}</td>
+                    <td>{post.title.length}</td>
+                </tr>
+            ))}
+        </tbody>    
+    </table>
+);
 
 export default Posts;
