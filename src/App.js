ba073ed34aa98969934a15ec6316fb76a79765a0
dgumpu <devgump07@gmail.com>
2018-07-19 09:47:54 -0500

    Implemented post page with bootstrap

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   
   render() {
     return (
-      <Posts />
+      <div className="container">
+        <Posts />
+      </div>
     );
   }
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,40 @@
-import React, { Component } from 'react';;
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Posts extends Component {
+    state = { posts: [] };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(
+            (posts) => this.setState({ posts: posts.data }));
+    }
+
     render() {
         return (
-            <h1>Possts</h1>
+            <div>
+                <h1>Post Samples</h1>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>User Id</th>
+                            <th>Size</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.posts.map(post => (
+                            <tr>
+                                <td>{post.id}</td>
+                                <td>{post.title}</td>
+                                <td>{post.userId}</td>
+                                <td>{post.title.length}</td>
+                            </tr>
+                        ))}
+                    </tbody>    
+                </table>
+                
+            </div>
         );
     }
 }
